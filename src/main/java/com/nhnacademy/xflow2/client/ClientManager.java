c0d180288cc088cb4f6d7c04cc198a34162fbc7a
pass0210 <pass0210@gmail.com>
2023-12-13 10:41:01 +0900

    Refactor(ClientManager) : URI마다 client 생성하게 수정

--- src/main/java/com/nhnacademy/xflow2/client/ClientManager.java
+++ src/main/java/com/nhnacademy/xflow2/client/ClientManager.java
@@ -1,44 +1,49 @@
 package com.nhnacademy.xflow2.client;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttSecurityException;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ClientManager {
-    MqttClient subClient;
-    MqttClient pubClient;
+    private static Map<String, MqttClient> map = new HashMap<>();
 
-    public ClientManager(String subURI, String pubURI) {
-        try {
-            subClient = new MqttClient(subURI, UUID.randomUUID().toString());
-            pubClient = new MqttClient(pubURI, UUID.randomUUID().toString());
-        } catch (MqttException e) {
-            log.error("mqtt error: {}", e.getMessage());
+    private ClientManager() {
+        throw new IllegalStateException();
+    }
+
+    public static MqttClient getClient(String serverURI) {
+        if (map.containsKey(serverURI)) {
+            return map.get(serverURI);
+        } else {
+            MqttClient client = null;
+            try {
+                client = new MqttClient(serverURI, UUID.randomUUID().toString());
+                setOption(client);
+                map.put(serverURI, client);
+            } catch (MqttException e) {
+                log.error("mqtt error: {}", e.getMessage());
+            }
+            return client;
         }
     }
 
-    public void mqttConnect() {
+    private static void setOption(MqttClient client) {
         MqttConnectOptions options = new MqttConnectOptions();
         options.setAutomaticReconnect(true);
-
         try {
-            subClient.connect(options);
-            pubClient.connect(options);
+            client.connect(options);
+        } catch (MqttSecurityException e) {
+            log.error("mqtt security error: {}", e.getMessage());
         } catch (MqttException e) {
             log.error("mqtt error: {}", e.getMessage());
         }
-    }
-
-    public MqttClient getSubClient() {
-        return subClient;
-    }
-
-    public MqttClient getPubClient() {
-        return pubClient;
     }
 }

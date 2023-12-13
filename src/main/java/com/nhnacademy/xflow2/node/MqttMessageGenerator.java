00f80d3ab26b55bc8924916372a066bd5a7b92d5
pass0210 <pass0210@gmail.com>
2023-12-13 11:10:41 +0900

    Refactor(MqttMessageGenerator) : 로직 수정

---
+++ src/main/java/com/nhnacademy/xflow2/node/MqttMessageGenerator.java
@@ -0,0 +1,39 @@
+package com.nhnacademy.xflow2.node;
+
+import org.json.JSONObject;
+
+import com.nhnacademy.xflow2.CommonsTopicGenerator;
+import com.nhnacademy.xflow2.message.JSONMessage;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MqttMessageGenerator extends InputOutputNode<JSONMessage, JSONMessage> {
+    CommonsTopicGenerator topicGenerator;
+
+    protected MqttMessageGenerator(int inputCount, int outputCount) {
+        super(inputCount, outputCount);
+        topicGenerator = new CommonsTopicGenerator();
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                JSONMessage message = tryGetMessage();
+                JSONObject object = message.getPayload();
+
+                JSONObject o = new JSONObject();
+                o.put("topic", topicGenerator.generate(object).toString());
+                o.put("time", System.currentTimeMillis() / 1000L);
+                o.put("value", (object.optString("value")));
+                log.info(o.toString());
+                output(0, new JSONMessage(o));
+            } catch (InterruptedException e) {
+                log.error("interrupt error: {}", e.getMessage());
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+}

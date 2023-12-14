365ed06ee90b98bda38fb8647c0d983692b98b29
pass0210 <pass0210@gmail.com>
2023-12-14 12:59:48 +0900

    Test : junit version 변경

--- src/test/java/com/nhnacademy/xflow2/node/ModbusInTest.java
+++ src/test/java/com/nhnacademy/xflow2/node/ModbusInTest.java
@@ -1,13 +1,14 @@
 package com.nhnacademy.xflow2.node;
 
-import org.junit.Test;
-
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import org.junit.jupiter.api.Test;
+
 public class ModbusInTest {
     private final int DEFAULT_PORT = 1512;
+
     @Test
     public synchronized void connectionTest() throws IOException, InterruptedException {
         ServerSocket serverSocket = new ServerSocket(DEFAULT_PORT);
@@ -17,7 +18,7 @@ public class ModbusInTest {
 
         Socket socket = new Socket("localhost", DEFAULT_PORT);
 
-        byte[] bytes = {0, 0, 0, 0, 0};
+        byte[] bytes = { 0, 0, 0, 0, 0 };
         socket.getOutputStream().write(bytes);
         socket.getOutputStream().flush();
 
